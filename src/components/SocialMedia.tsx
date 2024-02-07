b805c52ae7ad0ba28cfbe8be1fbd51ce783b28d1
Chetraru Ion <neluchetraru@gmail.com>
2024-02-07 09:05:10 +0100

    deleted instagram from SM

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,48 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 interface Props {
-    className: string;
+  className: string;
 }
 
-
-const SocialMedia = ({ className } : Props) => {
-    const socialMedia = [
-        {
-            name: "GitHub",
-            url: "https://github.com/neluchetraru",
-            icon: faGithub,
-        },
-        {
-            name: "LinkedIn",
-            url: "https://www.linkedin.com/in/chetraru-ion",
-            icon: faLinkedin,
-        },
-        {
-            name: "Instagram",
-            url: "https://www.instagram.com/chetraru_ion/",
-            icon: faInstagram,
-        }
-
-    ];
-    return (
-        <div className={className}>
-            {socialMedia.map((social) => (
-                social.url && (
-                    <a
-                        key={social.name}
-                        href={social.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mr-4 block hover:text-slate-300 text-slate-400"
-                    >
-                        <FontAwesomeIcon icon={social.icon} size="xl" color="" />
-                    </a>
-                ))   
-            )}
-        </div>
-    )
-}
-
+const SocialMedia = ({ className }: Props) => {
+  const socialMedia = [
+    {
+      name: "GitHub",
+      url: "https://github.com/neluchetraru",
+      icon: faGithub,
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/chetraru-ion",
+      icon: faLinkedin,
+    },
+  ];
+  return (
+    <div className={className}>
+      {socialMedia.map(
+        (social) =>
+          social.url && (
+            <a
+              key={social.name}
+              href={social.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mr-4 block hover:text-slate-300 text-slate-400"
+            >
+              <FontAwesomeIcon icon={social.icon} size="xl" color="" />
+            </a>
+          )
+      )}
+    </div>
+  );
+};
 
 export default SocialMedia;
